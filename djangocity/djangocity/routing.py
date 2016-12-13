f94fc84d1784bcc5fe8d88aff1d5d1e0c7fcd872
Kenneth Love <kennethlove@gmail.com>
2016-12-13 11:24:29 -0800

    Basic Channels setup

---
+++ djangocity/djangocity/routing.py
@@ -0,0 +1,5 @@
+from channels.routing import route
+
+channel_routing = [
+
+]
--- djangocity/djangocity/settings.py
+++ djangocity/djangocity/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'votes',
 ]
 
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgiref.inmemory.ChannelLayer",
+        "ROUTING": "djangocity.routing.channel_routing"
+    }
+}
